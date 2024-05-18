9ec41c94d3af9d0573bc08c5802a35e88004b7b4
Shuru63 <shubhamkuargarg63@gmail.com>
2024-05-18 23:55:34 +0530

    add the backend and frotend

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,34 @@
-const app=require("./app");
-const dotenv=require("dotenv");
-const path=require("path");
-const Connectdb=require("../Backend/database/database")
-const configPath = path.resolve(__dirname, 'Config/config.env');
-dotenv.config({ path: configPath });
-// process.on("unhandleREjection",(err)=>{
-//     console.log(`error:${err.message}`);
-//     console.log(`Shutting down the server becouse of hundle rejection`);
-//     process.exit(1);
-// })
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-// database connect
-Connectdb();
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./SocketIO/server.js";
 
-const server=app.listen(process.env.PORT,()=>{
-    console.log(`server is runnimg : /http//:localhost:${process.env.PORT}`)
-})
+dotenv.config();
 
-process.on("unhandle error",(err)=>{
-    console.log(`error:${err.message}`);
-    console.log(`Shutting down the server becouse of hundle rejection`);
-    server.close(()=>{
-        process.exit(1);
-    })
-})
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+const URI = process.env.MONGODB_URI;
+
+try {
+    mongoose.connect(URI);
+    console.log("Connected to MongoDB");
+} catch (error) {
+    console.log(error);
+}
+
+//routes
+app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
+
+server.listen(PORT, () => {
+    console.log(`Server is Running on port ${PORT}`);
+});
